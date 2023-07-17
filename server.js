cdeb307f157cf0236b4b0669165dc8a6eadf81ac
Roshan <shahroshan0720@gmail.com>
2023-07-17 15:10:01 +0530

    setup news cards

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 
+app.use(express.json());
 PORT = 3002;
 
 app.get('/',(req,res)=>{
     res.sendFile("index.html",{root:path.join(__dirname)});
+})
+
+app.get('/api',async(req,res)=>{
+    console.log("hi");
+
+    console.log(req._parsedUrl.query);
+    let data = await axios(`https://newsapi.org/v2/everything?${req._parsedUrl.query}&apiKey=73dccde54f984428a50909384654f006`)
+    let info = data.data;
+    //console.log(info);
+    res.json(info);
 })
 
 app.listen(PORT,()=>{
